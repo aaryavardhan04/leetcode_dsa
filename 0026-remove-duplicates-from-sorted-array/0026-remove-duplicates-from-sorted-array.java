c76a145c0082db772af91dcef7a545c3e8e4d9f3
Aarya Vardhan Vankeepuram <vankeepuramaaryavardhan@gmail.com>
2026-07-31 10:04:45 +0530

    Time: 1 ms (77.25%), Space: 46.8 MB (24.86%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int k =0;
+
+        for (int i = 1 ; i < nums.length; i++){
+            if (nums[i]==nums[i-1]){
+                k++;
+            }
+            else{
+                nums[i-k]=nums[i];
+            }
+        }
+        k=nums.length-k;
+        return k;
+    }
+}
