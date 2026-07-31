a9443b28b03dac5ae4ed7d7202b81222ce065e07
Aarya Vardhan Vankeepuram <vankeepuramaaryavardhan@gmail.com>
2026-07-31 09:41:16 +0530

    Time: 5 ms (80.93%), Space: 46.2 MB (16.74%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,12 +1,11 @@
 class Solution {
     public boolean isPalindrome(int x) {
         int num = 0;
-        int temp=0;
-        int copy=x;
+        // int temp = 0;
+        int copy = x;
         while(copy !=0 && copy>0){
-            temp= copy%10;
+            num = num*10 +copy%10;
             copy /= 10;
-            num = num*10 + temp;
             // System.out.println(num);
         }
         if(x==num){
