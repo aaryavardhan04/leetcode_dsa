306da66da949977476283f763cd021e54b0fa38b
Aarya Vardhan Vankeepuram <vankeepuramaaryavardhan@gmail.com>
2026-08-05 20:54:41 +0530

    Time: 0 ms (100%), Space: 43.5 MB (43.36%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,27 +1,45 @@
+// class Solution {
+//     public int[] plusOne(int[] digits) {
+        
+//         if (digits[digits.length-1]<9){
+//             digits[digits.length-1]++;
+//         }
+//         else {
+//             int i = digits.length -1;
+//             while (digits[i]==9 ){
+//                 digits[i]=0;
+//                 i--;
+//                 if(i<0){
+//                     break;
+//                 }
+//             }
+//             if(i==-1){
+//                 int arr[]= new int[digits.length+1];
+//                 arr[0]=1;
+//                 return arr;
+//             }
+//             else{
+//                 digits[i]++;
+//             }
+//         }
+//         return digits;
+
+
+        
+//     }
+// }
+
 class Solution {
     public int[] plusOne(int[] digits) {
-        
-        if (digits[digits.length-1]<9){
-            digits[digits.length-1]++;
-        }
-        else {
-            int i = digits.length -1;
-            while (digits[i]==9 ){
-                digits[i]=0;
-                i--;
-                if(i<0){
-                    break;
-                }
-            }
-            if(i==-1){
-                int arr[]= new int[digits.length+1];
-                arr[0]=1;
-                return arr;
-            }
-            else{
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
                 digits[i]++;
+                return digits;
             }
+            digits[i] = 0;
         }
-        return digits;
+        int[] ans = new int[digits.length + 1];
+        ans[0] = 1;
+        return ans;
     }
 }
